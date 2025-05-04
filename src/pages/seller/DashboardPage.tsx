ea70dcf2dd2ab4eb075bfd3d9ad9c87fc561cb84
AnasBaqai <anashabib139@gmail.com>
2025-05-04 17:31:08 +0200

    remove unnacessary import

--- src/pages/seller/DashboardPage.tsx
+++ src/pages/seller/DashboardPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAppSelector } from "../../store/hooks";
 import Navbar from "../../components/layout/Navbar/Navbar";
